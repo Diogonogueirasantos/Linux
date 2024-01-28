2d652e9b68c1ee8461971cd64683e3d561a1395f
Diogo <diogo.nogueira.santos@outlook.com>
2024-01-28 15:58:12 -0300

    Alterações no script

--- Scripts/upgrade.sh
+++ Scripts/upgrade.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
 echo "Bem vindo de volta Diogo!"
-echo "1 opcao. Deseja atualizar os programas via sistema dnf e flatpak?"
-echo "2 opcao. Deseja apenas atualizar via dnf? "
-echo "3 opcao. Deseja apenas atualizar via flatpak? "
-
+echo "1. Deseja atualizar os programas via sistema dnf e flatpak?"
+echo "2. Deseja apenas atualizar via dnf? "
+echo "3. Deseja apenas atualizar via flatpak? "
+echo "4. Deseja instalar algum pacote? "
 op1=1
 op2=2
 op3=3
- 
+op4=4 
 read opcao
 
 if [ $opcao == $op1 ]; then
     sudo dnf up; flatpak upgrade
 elif [ $opcao == $op2 ]; then
-    python -c 'import os; os.system("sudo dnf up")'
+      sudo dnf up
 elif [ $opcao == $op3 ]; then
-    python -c 'import os; os.system("flatpak upgrade")' 
+      flatpak upgrade 
+elif [ $opcao == $op4 ]; then
+     echo "Por favor digite o nome do pacote: "
+     read pesquisa 
+     sudo dnf install $pesquisa
 else
    echo "Opção inválida"
 fi
